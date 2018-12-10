dda9cc63eded453db97af6adcb5ee8da12c984e4
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-12-10 11:15:46 -0800

    connecting the App to the store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './components/app/App';
+import { Provider } from 'react-redux';
+import store from './store';
 
-const helloWorld = <h1>Hello World</h1>;
 
 render(
-  helloWorld,
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 );

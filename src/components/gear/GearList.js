59f9c1a716e8088c145a4aacc2f32441d0536a1b
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-12-10 11:31:43 -0800

    component shows inital state

--- src/components/gear/GearList.js
+++ src/components/gear/GearList.js
@@ -1,10 +1,18 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 
 export default class GearList extends PureComponent {
+  static propTypes = {
+    list: PropTypes.array.isRequired,
+    fetch: PropTypes.func.isRequired
+  };
+
   render() {
+    const { list } = this .props;
+
     return (
       <div>
-        <h1>THIS IS THE GEAR PAGE!</h1>
+        {list.map(({ name }, i) => <h2 key={i}>{name}</h2>)}
       </div>
     );
   }
